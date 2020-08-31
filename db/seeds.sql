c174f9d6e6bd3249842a2dbd8e48e42a964b0404
Sheldon Mall <sheldoniznow@gmail.com>
2020-08-31 19:15:55 -0400

    First commit of the HW

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("BIG Mac", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Junior Chicken", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Fish-O-Fillet", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Aloo Tikki", true);
